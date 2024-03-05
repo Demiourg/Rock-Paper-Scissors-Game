e0acae5a83cbd5326fabd1df2f7123307e3713e2
Demiourg <george.alexiou@tutanota.com>
2024-03-05 19:45:42 +0100

    add if statements and return

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const buttons = document.querySelectorAll("button");
 
+const resultEl = document.getElementById("result");
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    console.log("user choice!", button.id, "computer choice!", computerPlay());
+    const result = playRound(button.id, computerPlay());
+    resultEl.textContent = result;
   });
 });
 
@@ -10,4 +13,18 @@ function computerPlay() {
   const choices = ["rock", "paper", "scissors"];
   const randomChoice = Math.floor(Math.random() * choices.length);
   return choices[randomChoice];
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
 }
